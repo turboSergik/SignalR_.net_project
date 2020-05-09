459b8f6fb226be875777bed12a08350ec42374a2
TurboSergik <serg.piskun111@mail.ru>
2020-05-09 23:49:44 +0300

    update chats

--- JobSolution/JobSolution.Repository/Concrete/ChatRepository.cs
+++ JobSolution/JobSolution.Repository/Concrete/ChatRepository.cs
@@ -10,12 +10,14 @@ namespace JobSolution.Repository.Concrete
     {
         public Task SendMessage(string jwt, string message)
         {
-            Console.WriteLine(Context.UserIdentifier);
+            Console.WriteLine(Context.UserIdentifier + " len=" + Context.Items.Count.ToString() + "  == " + Context.UserIdentifier);
             return Clients.All.SendAsync("ReceiveMessage", jwt, message);
         }
         public override async Task OnConnectedAsync()
         {
             Console.WriteLine("-------------------------conn");
+            
+
             await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
             await base.OnConnectedAsync();
         }
